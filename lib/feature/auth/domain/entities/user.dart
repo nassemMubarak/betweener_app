dfea5939a733dd565e2f5966f811c3a9d0926dee
Nassem <nassemmubarak@gmail.com>
2023-07-11 09:45:49 +0300

    update feature remote data source

--- lib/feature/auth/domain/entities/user.dart
+++ lib/feature/auth/domain/entities/user.dart
@@ -1,10 +1,17 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
+  final int id;
   final String name;
   final String email;
-  final int id;
   final String token;
+  final String? email_verified_at;
+  final int? isActive;
+  final String? country;
+  final String? ip;
+  final double? long;
+  final double? lat;
+  final String? fcm;
   final String updated_at;
   final String created_at;
 
@@ -15,15 +22,29 @@ class User extends Equatable {
     required this.id,
     required this.updated_at,
     required this.created_at,
+    this.email_verified_at,
+    this.isActive,
+    this.country,
+    this.ip,
+    this.long,
+    this.lat,
+    this.fcm,
   });
 
   @override
   List<Object?> get props => [
         name,
         email,
-        updated_at,
-        created_at,
         token,
         id,
+        updated_at,
+        created_at,
+        email_verified_at,
+        isActive,
+        country,
+        ip,
+        long,
+        lat,
+        fcm,
       ];
 }
